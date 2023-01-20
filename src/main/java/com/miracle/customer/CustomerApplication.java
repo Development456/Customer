85fdb14db64c9f8b471eb4b94b03efca6249a0b6
Alakshender Kumar <ramakant987@gmail.com>
2023-01-20 12:12:43 -0500

    Circuit Breaker

--- src/main/java/com/miracle/customer/CustomerApplication.java
+++ src/main/java/com/miracle/customer/CustomerApplication.java
@@ -1,3 +1,7 @@
+/*******
+* Copyright (C) 2023 Claims Application-Miracle Software Systems Inc
+* All Rights Reserved.
+*******/
 package com.miracle.customer;
 
 import org.springframework.boot.SpringApplication;
--- src/main/java/com/miracle/customer/repository/CustomerRepository.java
+++ src/main/java/com/miracle/customer/repository/CustomerRepository.java
@@ -1,3 +1,7 @@
+/*******
+* Copyright (C) 2023 Claims Application-Miracle Software Systems Inc
+* All Rights Reserved.
+*******/
 package com.miracle.customer.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
