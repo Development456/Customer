c7b3acd0d5534d87b8fed496754908222343a0d0
Abhishek Gujamagdi <abhigujamagdi@gmail.com>
2023-02-02 19:42:42 -0500

    Spring vault

---
+++ src/main/java/com/miracle/customer/config/ConfigurationDetails.java
@@ -0,0 +1,38 @@
+package com.miracle.customer.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties("customer")
+@Configuration
+public class ConfigurationDetails {
+	private String host;
+	private String database;
+	private int port;
+	private String value;
+	public String getHost() {
+		return host;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public void setHost(String host) {
+		this.host = host;
+	}
+	public String getDatabase() {
+		return database;
+	}
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+}
+
